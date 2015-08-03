d754a4bd7f8e7042cd370bf50dea3410fffc05a3
denlysenko <den_2710@ukr.net>
2015-08-03 20:29:22 +0300

    Update secure.js

--- config/env/secure.js
+++ config/env/secure.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	port: 443,
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/my-book-app',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || '***',
 	assets: {
 		lib: {
 			css: [
