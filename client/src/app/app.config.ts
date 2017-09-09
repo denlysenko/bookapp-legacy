7c6cd73baa060e76f80cf23e0c617029f091f216
den.lysenko <den.l@april33.com>
2017-09-09 15:24:11 +0300

    updated deps

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,7 +1,7 @@
-import { OpaqueToken } from '@angular/core';
+import { InjectionToken } from '@angular/core';
 import { IAppConfig } from "./models/AppConfig";
 
-export let APP_CONFIG = new OpaqueToken('app.config');
+export let APP_CONFIG = new InjectionToken<IAppConfig>('app.config');
 
 export const AppConfig: IAppConfig = {
   baseUrl: 'http://bookappserver-den2710.rhcloud.com'
